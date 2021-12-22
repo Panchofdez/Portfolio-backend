e1265cef448ed308ca64369dd692f9399d38dab0
Pancho Fernandez <panchofern@gmail.com>
2021-12-22 00:37:52 -0500

    Updated index.js to handle cors issue

--- index.js
+++ index.js
@@ -35,6 +35,11 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
